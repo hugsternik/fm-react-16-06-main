6fcc681d7f00a2e5da3ae1eca9362b7ae237850e
hugster <niki.struk@gmail.com>
2022-06-23 11:52:55 +0300

    szd

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import React, { Component } from "react";
+import StopWatch from "./components/StopWatch";
+
+
+const App = () => {
+  return <>
+    <StopWatch />
+  </>;
+};
+export default App;
